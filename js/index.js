383c3af49bd1926f70fe27a95ea439fffdd2f85a
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-04-13 13:03:55 -0400

    added 3 event listeners to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+const navLinks = document.querySelector(".nav");
+navLinks.addEventListener('mouseover', function(e) {
+  e.target.style.color = 'red';
+  setTimeout(function() {
+    e.target.style.color = '';
+  }, 500)
+}, false);
+
+const body = document.querySelector('body')
+body.addEventListener('keydown', function(e) {
+  console.log(e.key)
+})
+
+const btnAlerts = document.querySelectorAll('.btn')
+btnAlerts.forEach((btn, i) => {
+  btn.addEventListener('dblclick', function(e) {
+    alert(`You double clicked button #${i + 1}`)
+  })
+})
